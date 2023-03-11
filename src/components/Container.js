4b90b70951c5f2a83bb2578e7e05c539f3cdf7f2
katarinjah <katarina.d.262@gmail.com>
2023-03-11 21:48:48 +0100

    List layout done

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,10 +1,12 @@
 import "./Container.css";
 import { UserGrid } from "./UserGrid";
+// import { UserList } from "./UserList";
 
 export const Container = () => {
     return (
         <div className="container-fluid">
             <UserGrid />
+            {/* <UserList /> */}
         </div>
     );
 }
